a1c35cd28943c319dfeabb7db91ae81b0f8717f2
natsukinohikari <blancayfran55@gmail.com>
2026-03-02 20:01:18 +0100

    Aprendizaje del emit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import TaskInput from './components/TaskInput.vue'
+
+const listaTareas = ref([])
+
+function agregarTareaHijo(tarea) {
+  listaTareas.value.push({
+    id: Date.now(),
+    texto: tarea
+  })
+}
+
+function borrarTarea(indice) {
+  listaTareas.value.splice(indice, 1)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <main>
-    <TheWelcome />
+    <TaskInput @anadir-tarea="agregarTareaHijo"/>
+
+    <ul v-if="listaTareas.length > 0" >
+      <li v-for="(tarea, indice) in listaTareas" :key="tarea.id">
+        {{ tarea.texto }}
+        <button @click="borrarTarea(indice)">BORRAR TAREA</button>
+      </li>
+    </ul>
+
+    <p v-else>No hay tareas para mostrar</p>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/TaskInput.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const tarea = ref('')
+const emit = defineEmits(['anadir-tarea'])
+
+function anadirTarea() {
+    if (tarea.value.trim() == '') return
+    emit('anadir-tarea', tarea.value)
+    tarea.value = ""
+}
+</script>
+
+<template>
+    <input type="text" v-model="tarea" placeholder="Escribe la tarea a enviar al padre"/>
+    <button @click="anadirTarea">AÑADIR TAREA</button>
+</template>
