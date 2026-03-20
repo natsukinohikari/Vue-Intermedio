4f55bb2e0a3b0fba04104995ab80efde227e3ba2
natsukinohikari <blancayfran55@gmail.com>
2026-03-20 18:45:08 +0100

    Adición de un transition y unos estilos para animaciones, así como su explicación en el README

--- src/components/Modals.vue
+++ src/components/Modals.vue
@@ -10,9 +10,23 @@ const emit = defineEmits(['cerrar-modal'])
 
 <template>
     <teleport to="#modal">
-        <div v-if="mostrarModal" class="modal">
-            <slot></slot>
-            <button @click="emit('cerrar-modal')">Cerrar modal</button>
-        </div>
+        <transition name="transicion_modal">
+            <div v-if="mostrarModal" class="modal">
+                <slot></slot>
+                <button @click="emit('cerrar-modal')">Cerrar modal</button>
+            </div>
+        </transition>
     </teleport>
 </template>
+
+<style>
+.transicion_modal-enter-active,
+.transicion_modal-leave-active {
+  transition: opacity 1s ease;
+}
+
+.transicion_modal-enter-from,
+.transicion_modal-leave-to {
+  opacity: 0;
+}
+</style>
